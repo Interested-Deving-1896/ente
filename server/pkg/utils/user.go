2290b5c42daf84b83a68bf2843d31a827ad78d2f
andrey <andrey@unplugged.com>
2025-11-30 14:08:33 +0200

    Downgrade log level from `Info` to `Debug` in `GetUserByEmailHash` for less verbose logging.

--- server/pkg/utils/user.go
+++ server/pkg/utils/user.go
@@ -19,7 +19,7 @@ func (c *User) GetUserID(upUsername string) (int64, string, error) {
 	var emailHash string
 	var user ente.User
 	emailHash, err = crypto.GetHash(upUsername, c.HashingKey)
-	log.Infof("Username: %s, emailHash: %s", upUsername, emailHash)
+	log.Debugf("Username: %s, emailHash: %s", upUsername, emailHash)
 	if err != nil {
 		return 0, "", stacktrace.Propagate(err, "failed to hash username")
 	}
@@ -28,11 +28,11 @@ func (c *User) GetUserID(upUsername string) (int64, string, error) {
 	if err != nil {
 		err = nil
 		// If user not found, try with email format (username@domain)
-		log.Infof("user not found c.UserRepo.GetUserByEmailHash(%s): %s", upUsername, err)
+		log.Debugf("user not found c.UserRepo.GetUserByEmailHash(%s): %s", upUsername, err)
 
 		upUsername = upUsername + "@" + viper.GetString("unplugged.email-host")
 		emailHash, err = crypto.GetHash(upUsername, c.HashingKey)
-		log.Infof("Username: %s, emailHash: %s", upUsername, emailHash)
+		log.Debugf("Username: %s, emailHash: %s", upUsername, emailHash)
 		if err != nil {
 			return 0, "", stacktrace.Propagate(err, "failed to hash email username")
 		}
@@ -42,6 +42,6 @@ func (c *User) GetUserID(upUsername string) (int64, string, error) {
 			return 0, "", stacktrace.Propagate(err, "failed to get user by emailUser hash")
 		}
 	}
-	log.Infof("user found c.UserRepo.GetUserByEmailHash(emailHash): %s, %s", user.ID, upUsername)
+	log.Debugf("user found c.UserRepo.GetUserByEmailHash(emailHash): %s, %s", user.ID, upUsername)
 	return user.ID, upUsername, nil
 }
